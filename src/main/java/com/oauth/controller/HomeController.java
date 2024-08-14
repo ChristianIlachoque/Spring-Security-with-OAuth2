6b323c1f6ed1ed6ec82511e8b7e8960a467b45d6
ChristianIlachoque <cilachoque@unsa.edu.pe>
2024-08-14 08:30:06 -0500

    first commit

---
+++ src/main/java/com/oauth/controller/HomeController.java
@@ -0,0 +1,17 @@
+package com.oauth.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello World!!!";
+    }
+
+    @GetMapping("/hello-secured")
+    public String helloSecured(){
+        return "Hello World Secured!!";
+    }
+}
